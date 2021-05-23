73e2a82a0a51a4c62c6546a5e74a4519ec4513f8
swartsd <swa17001@byui.edu>
2021-05-22 21:50:44 -0600

    Add my Name: Dallin

--- src/bjork/Main.java
+++ src/bjork/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, my name is Elijah.");
+        System.out.println("Hello Elijah, my name is Dallin.");
 
     }
 }
